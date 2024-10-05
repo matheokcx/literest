50b4728763fdf34bed7bb8b4b35ce2ca4d29cc34
Deleplanque Matheo <matheo_deleplanque@ens.univ-artois.fr>
2024-10-06 00:58:49 +0200

    Avancées sur la page de connexione

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,15 +12,16 @@ export default function Home() {
   }
 
   return (
-    <html>
-      <div className="w-full h-full p-12 flex flex-row justify-center items-center bg-white text-black font-sans font-bold">
-        <img src="/" alt="Illustration" className="w-full h-full hover:w-1/2 rounded-3xl" />
-        <form onSubmit={() => connexion()}>
-          <input type="mail" value={mail} name="mail" onChange={(e: any) => setMail(e.target.value)} placeholder="Votre adresse e-mail" className="" />
-          <input type="password" value={motDePasse} name="password" onChange={(e: any) => setMotDePasse(e.target.value)} placeholder="Votre adresse e-mail" className="" />
-          <button type="submit" value="Connexion" className=""> </button>
-        </form>
+    <>
+      <div className="w-screen h-screen p-12 flex flex-row justify-center items-center gap-16 bg-white text-black font-sans">
+        <video src="/video.mp4" autoPlay muted loop className="w-7/12 h-full" />
+        <div className="w-5/12 flex flex-col items-center justify-center gap-16">
+          <h2 className="text-3xl font-bold">Literest - Log In</h2>
+          <input type="mail" value={mail} onChange={(e: any) => setMail(e.target.value)} placeholder="Votre adresse e-mail" className="w-3/4 h-8 rounded-2xl p-1 border-2 border-gray-400 bg-white text-black" />
+          <input type="password" value={motDePasse} onChange={(e: any) => setMotDePasse(e.target.value)} placeholder="Votre adresse e-mail" className="w-3/4 h-8 rounded-2xl p-1 border-2 border-gray-400 bg-white text-black" />
+          <button onClick={() => connexion()} className="w-1/4 h-10 rounded-xl bg-green-500 text-white font-bold transition-transform hover:scale-103 hover:-translate-y-1">Connexion</button>
+        </div>
       </div>
-    </html>
+    </>
   );
 }
